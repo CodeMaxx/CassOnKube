33b6f0fe5574d037950fa0aff103b5bc9fa08b58
Akash Trehan <akash.trehan123@gmail.com>
2019-02-25 02:12:58 +0530

    Docker installation script complete!

--- install_docker.sh
+++ install_docker.sh
@@ -3,15 +3,26 @@
 ### For Ubuntu 18.04
 #####
 
+RED='\033[0;31m'
+NC='\033[0m'
+
 if [ -x "$(command -v docker)" ]; then
     echo "Using " $(docker -v)
 else
-    echo "Installing docker..."
-    sudo apt update
-    sudo apt install -v apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-    sudo apt update
-    sudo apt install -y docker-ce
+    echo -e "${RED}Installing docker...${NC}"
+    echo "Updating Packages..."
+    apt update
+    echo "Installing Pre-requisites..."
+    apt install -v apt-transport-https ca-certificates curl software-properties-common && \\
+    echo "Adding Docker repository..."
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \\
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" && \\
+    apt update
+    apt install -y docker-ce
+    if [ -x "$(command -v docker)" ]; then
+        echo -e "${RED}" $(docker -v) " Installed!${NC}"
+    else
+        echo -e "${RED}Could not install Docker!${NC}"
+        exit 1
+    fi
 fi
-
