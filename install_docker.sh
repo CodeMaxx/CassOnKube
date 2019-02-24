bc264b52a25c2c1b477ce1feb8c6e7234faa5139
Akash Trehan <akash.trehan123@gmail.com>
2019-02-25 02:12:58 +0530

    Docker installation script complete!

--- install_docker.sh
+++ install_docker.sh
@@ -3,15 +3,31 @@
 ### For Ubuntu 18.04
 #####
 
-if [ -x "$(command -v docker)" ]; then
-    echo "Using " $(docker -v)
-else
-    echo "Installing docker..."
-    sudo apt update
-    sudo apt install -v apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-    sudo apt update
-    sudo apt install -y docker-ce
-fi
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
+if [ -x "$(command -v docker)" ]; then
+    echo "${GREEN}Using " $(docker -v) '${NC}'
+else
+    echo "${GREEN}Installing Docker...${NC}"
+    echo "${BLUE}Updating Packages...${NC}"
+    apt update && \
+    echo "${BLUE}Installing Pre-requisites...${NC}" && \
+    apt install -v apt-transport-https ca-certificates curl software-properties-common && \
+    echo "${BLUE}Adding Docker repository...${NC}" && \
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" && \
+    apt update && \
+    echo "${BLUE}Beginning docker Installation...${NC}"
+    apt install -y docker-ce
+    if [ -x "$(command -v docker)" ]; then
+        echo "${GREEN}" $(docker -v) " Installed!${NC}"
+        exit 0
+    else
+        echo "${RED}Could not install Docker!"
+        echo "Aborting..."
+        exit 1
+    fi
+fi
